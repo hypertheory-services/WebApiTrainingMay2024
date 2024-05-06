3532b47d1583273ff96f5fbeb97f0aac5f9242a9
Jeff Gonzalez <jeff@hypertheory.com>
2024-05-06 14:53:40 -0400

    can add stuff

--- src/IssueTrackerSolution/IssueTracker.Api/Catalog/Api.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Catalog/Api.cs
@@ -1,21 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTracker.Api.Catalog;
 
-public class Api : ControllerBase
+public class Api(IValidator<CreateCatalogItemRequest> validator, IDocumentSession session) : ControllerBase
 {
+
     [HttpPost("/catalog")]
     public async Task<ActionResult> AddACatalogItemAsync(
         [FromBody] CreateCatalogItemRequest request,
         CancellationToken token)
     {
-        // get the JSON data they sent and look at it. Is it cool?
-        // If not, send them an error (400, with some details)
-        // if it is cool, maybe save it to a database or something?
-        // and what are we going to return.
+        var validation = await validator.ValidateAsync(request, token);
+        if (!validation.IsValid)
+        {
+            return this.CreateProblemDetailsForModelValidation("Cannot Add Catalog Item", validation.ToDictionary());
+        }
 
-        var response = new CatalogItemResponse(Guid.NewGuid(), request.Title, request.Description);
-        return Ok(response);
+        var entityToSave = new CatalogItem(Guid.NewGuid(), request.Title, request.Description, "todo", DateTimeOffset.Now);
+        session.Store(entityToSave);
+        await session.SaveChangesAsync(); // Do the actual work!
+
+
+        var response = new CatalogItemResponse(entityToSave.Id, request.Title, request.Description);
+        return Ok(response); // I have stored this thing in such a way that you can get it again, it is now
+        // part of this collection. 
     }
 }
 
--- src/IssueTrackerSolution/IssueTracker.Api/Program.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Program.cs
@@ -1,11 +1,28 @@
+using FluentValidation;
+using IssueTracker.Api.Catalog;
+using Marten;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSingleton<IValidator<CreateCatalogItemRequest>, CreateCatalogItemRequestValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateCatalogItemRequestValidator>();
+
+var connectionString = builder.Configuration.GetConnectionString("data") ?? throw new Exception("Can't start, need a connection string");
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+}).UseLightweightSessions();
+
 
 var app = builder.Build();
 
