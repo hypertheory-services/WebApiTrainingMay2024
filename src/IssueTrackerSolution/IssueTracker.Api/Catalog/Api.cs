fcda2391389a1743604108b8e7f0c40cbcfdf12d
Jeff Gonzalez <jeff@hypertheory.com>
2024-05-06 12:31:23 -0400

    before lunch

---
+++ src/IssueTrackerSolution/IssueTracker.Api/Catalog/Api.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTracker.Api.Catalog;
+
+public class Api : ControllerBase
+{
+    [HttpPost("/catalog")]
+    public async Task<ActionResult> AddACatalogItemAsync(
+        [FromBody] CreateCatalogItemRequest request,
+        CancellationToken token)
+    {
+        // get the JSON data they sent and look at it. Is it cool?
+        // If not, send them an error (400, with some details)
+        // if it is cool, maybe save it to a database or something?
+        // and what are we going to return.
+
+        var response = new CatalogItemResponse(Guid.NewGuid(), request.Title, request.Description);
+        return Ok(response);
+    }
+}
+
---
+++ src/IssueTrackerSolution/IssueTracker.Api/Catalog/Models.cs
@@ -0,0 +1,8 @@
+﻿namespace IssueTracker.Api.Catalog;
+
+// "Models" are the things that leave or arrive from outside our service boundary.
+// they are C# types that will be deserialized or serialized from the json coming into our out of our api.
+
+public record CreateCatalogItemRequest(string Title, string Description);
+
+public record CatalogItemResponse(Guid Id, string Title, string Description);
